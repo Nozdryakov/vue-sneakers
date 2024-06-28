dad8926258795d7b1fafed8c202fe033bdaf1610
Nozdryakov <nozdryakovtema0603@gmail.com>
2024-06-28 18:07:38 +0300

    Создал проект и header

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+  <Header></Header>
+    <h1>Sneakers</h1>
+  </div>
+</template>
+<script setup>
+import Header from "@/components/Header.vue";
+</script>
