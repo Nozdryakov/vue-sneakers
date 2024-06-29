bc34bcd289a42d577add089af9836e214638b34f
Nozdryakov <nozdryakovtema0603@gmail.com>
2024-06-29 21:51:43 +0300

    Сортировка и Поиск

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="grid grid-cols-4 gap-5">
     <card
-        image-url="/sneakers/sneakers-1.jpg"
-        title="Hello"
-        :price="5000"
+        v-for="item in items"
+        :key="item.id"
+        :image-url="item.imageUrl"
+        :title="item.title"
+        :price="item.price"
         :is-favorite="true"
         :on-click-add="onClickAdd"
     ></card>
-    <card image-url="/sneakers/sneakers-2.jpg"></card>
-    <card image-url="/sneakers/sneakers-3.jpg"></card>
-    <card></card>
-    <card></card>
-    <card></card>
   </div>
 </template>
 <script setup>
 import Card from "@/components/Card.vue";
 
+defineProps({
+  items: Array
+})
 const onClickAdd = () => {
     alert('add');
 }
